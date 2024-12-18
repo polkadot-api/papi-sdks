5a4fdd03c64f0c2a9f7ee0375f1f64feaf4523ca
Carlo Sala <carlosalag@protonmail.com>
2024-12-18 13:33:35 +0100

    expose only few stuff from descriptors

---
+++ packages/sdk-xcm/src/descriptors.ts
@@ -0,0 +1,9 @@
+export { pas as relayDesc } from "./innerDescriptors"
+export { pah as ahDesc } from "./innerDescriptors"
+
+export {
+  XcmV3Junction,
+  XcmV3Junctions,
+  XcmV3JunctionNetworkId,
+  XcmVersionedLocation,
+} from "./innerDescriptors"
