42361b55d45e6dc7be588ee1d37b8d1cff72ebd7
Carlo Sala <carlosalag@protonmail.com>
2024-12-20 11:08:55 +0100

    simplify locations

--- packages/sdk-xcm/src/known-locations.ts
+++ packages/sdk-xcm/src/known-locations.ts
@@ -1,41 +1,24 @@
 import { XcmV3Junction } from "@/descriptors"
 import { Binary, Enum } from "polkadot-api"
 
+const PASEO: XcmV3Junction[] = [
+  XcmV3Junction.GlobalConsensus(
+    Enum(
+      "ByGenesis",
+      Binary.fromHex(
+        "0x77afd6190f1554ad45fd0d31aee62aacc33c6db0ea801129acb813f913e0764f",
+      ),
+    ),
+  ),
+]
+const PASEO_AH: XcmV3Junction[] = [...PASEO, XcmV3Junction.Parachain(1000)]
 export const CHAINS = {
-  paseo: [
-    XcmV3Junction.GlobalConsensus(
-      Enum(
-        "ByGenesis",
-        Binary.fromHex(
-          "0x77afd6190f1554ad45fd0d31aee62aacc33c6db0ea801129acb813f913e0764f",
-        ),
-      ),
-    ),
-  ] as XcmV3Junction[],
-  paseoAssetHub: [
-    XcmV3Junction.GlobalConsensus(
-      Enum(
-        "ByGenesis",
-        Binary.fromHex(
-          "0x77afd6190f1554ad45fd0d31aee62aacc33c6db0ea801129acb813f913e0764f",
-        ),
-      ),
-    ),
-    XcmV3Junction.Parachain(1000),
-  ] as XcmV3Junction[],
+  paseo: PASEO,
+  paseoAssetHub: PASEO_AH,
 }
 
 export const TOKENS = {
-  PAS: [
-    XcmV3Junction.GlobalConsensus(
-      Enum(
-        "ByGenesis",
-        Binary.fromHex(
-          "0x77afd6190f1554ad45fd0d31aee62aacc33c6db0ea801129acb813f913e0764f",
-        ),
-      ),
-    ),
-  ] as XcmV3Junction[],
+  PAS: PASEO,
 }
 
 export const TOKENS_IN_CHAINS = {
