51e733fdeaa09ee5c95bc3f1759350388d2d962e
Victor Oliva <olivarra1@gmail.com>
2025-06-04 16:06:11 +0200

    feat: get events when dry-running on revive

--- packages/sdk-ink/src/util.ts
+++ packages/sdk-ink/src/util.ts
@@ -1,4 +1,6 @@
-import { Enum } from "polkadot-api"
+import { keccak_256 } from "@noble/hashes/sha3"
+import { AccountId, Binary, Enum, SS58String } from "polkadot-api"
+import { U256 } from "./descriptor-types"
 
 export const getSignedStorage = (
   depositResponse: Enum<{
@@ -16,3 +18,14 @@ export const getStorageLimit = (
     Charge: bigint
   }>,
 ) => (depositResponse.type === "Charge" ? depositResponse.value : 0n)
+
+export const ss58ToEthereum = (address: SS58String) =>
+  Binary.fromBytes(keccak_256(AccountId().enc(address)).slice(12))
+
+const u64Range = 2n ** 64n
+export const valueToU256 = (value: bigint): U256 => [
+  value % u64Range,
+  (value / u64Range) % u64Range,
+  (value / u64Range ** 2n) % u64Range,
+  (value / u64Range ** 3n) % u64Range,
+]
