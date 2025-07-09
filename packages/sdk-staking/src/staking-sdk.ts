1607b44c048d37c95cf3b5623d351fe86e613c1e
Victor Oliva <olivarra1@gmail.com>
2025-07-09 12:49:51 +0200

    cache ErasStakersPaged

--- packages/sdk-staking/src/staking-sdk.ts
+++ packages/sdk-staking/src/staking-sdk.ts
@@ -1,3 +1,4 @@
+import { SS58String } from "polkadot-api"
 import { StakingSdkTypedApi } from "./descriptors"
 import { StakingSdk } from "./sdk-types"
 
@@ -11,25 +12,73 @@ export function createStakingSdk(
   api: StakingSdkTypedApi,
   _settings: { maxActiveNominators: number },
 ): StakingSdk {
+  // Values from ErasStakersPaged can't change after it has become the active era.
+  // Papi caches per-block, but as we know this can't change and it's an expensive request, we set up the cache in here.
+  let activeEraPromise = api.query.Staking.ActiveEra.getValue().then(
+    (val) => val!.index,
+  )
+  const erasStakersCache: Record<
+    number,
+    Promise<
+      Array<{
+        keyArgs: [number, SS58String, number]
+        value: {
+          page_total: bigint
+          others: Array<{
+            who: SS58String
+            value: bigint
+          }>
+        }
+      }>
+    >
+  > = {}
+  const getEraStakers = (era: number) => {
+    if (era in erasStakersCache) return erasStakersCache[era]
+
+    const result = api.query.Staking.ErasStakersPaged.getEntries(era)
+
+    // It's safe for others to use this cache temporarily
+    erasStakersCache[era] = result
+
+    // Remove from cache if it's actually above the active era
+    activeEraPromise.then(async (activeEra) => {
+      if (activeEra >= era) return
+
+      // refresh just-in-case it has changed in a recent block
+      activeEraPromise = api.query.Staking.ActiveEra.getValue().then(
+        (val) => val!.index,
+      )
+      if ((await activeEraPromise) < era) {
+        delete erasStakersCache[era]
+      }
+    })
+
+    return result
+  }
+
   const getNominatorStatus: StakingSdk["getNominatorStatus"] = async (
     addr,
     era,
   ) => {
+    const selectedEra = era ? Promise.resolve(era) : activeEraPromise
+    const eraEntries = selectedEra.then(getEraStakers)
+
     const [activeEra, depth] = await Promise.all([
-      api.query.Staking.ActiveEra.getValue().then((val) => val!.index),
+      activeEraPromise,
       api.constants.Staking.HistoryDepth(),
     ])
-    era ??= activeEra
+    era = await selectedEra
     assertEra(activeEra, depth, era)
-    const active = (
-      await api.query.Staking.ErasStakersPaged.getEntries(era)
-    ).flatMap(({ keyArgs: [, validator], value: { others } }) =>
-      others
-        .filter(({ who }) => who === addr)
-        .map(({ value }) => ({ validator, activeBond: value })),
+
+    const active = (await eraEntries).flatMap(
+      ({ keyArgs: [, validator], value: { others } }) =>
+        others
+          .filter(({ who }) => who === addr)
+          .map(({ value }) => ({ validator, activeBond: value })),
     )
     return { era, active }
   }
+
   // TODO: include VoterList check
   const canNominate: StakingSdk["canNominate"] = async (addr) => {
     const [{ data }, ledger, minBond, ed] = await Promise.all([
@@ -46,6 +95,7 @@ export function createStakingSdk(
     if (!canNominate) return { canNominate }
     return { canNominate, maxBond, currentBond: ledger?.total ?? 0n }
   }
+
   return {
     getNominatorStatus,
     canNominate,
