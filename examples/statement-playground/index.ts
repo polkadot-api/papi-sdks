be10cdf4eab1f1400945648f24d0ea102135694d
Carlo Sala <carlosalag@protonmail.com>
2025-12-05 11:57:53 +0100

    some testing around

--- examples/statement-playground/index.ts
+++ examples/statement-playground/index.ts
@@ -7,13 +7,13 @@ import {
 } from "@polkadot-api/sdk-statement"
 import { getWsProvider } from "@polkadot-api/ws-provider"
 import { sign, getPublicKey } from "@scure/sr25519"
-import { fromHex, jsonPrint } from "@polkadot-api/utils"
-import { Binary } from "@polkadot-api/substrate-bindings"
+import { fromHex, jsonPrint, mergeUint8 } from "@polkadot-api/utils"
+import { Binary, Blake2256 } from "@polkadot-api/substrate-bindings"
 
 // use any key
 const ALICE_SK = fromHex(process.env.ALICE_SK!)
 const alice = getStatementSigner(getPublicKey(ALICE_SK), "sr25519", (p) =>
-  sign(ALICE_SK, p),
+  sign(ALICE_SK, p, Blake2256(mergeUint8([ALICE_SK, p]))),
 )
 
 const client = createClient(getWsProvider("ws://127.0.0.1:9936"))
@@ -23,13 +23,14 @@ const sdk = createStatementSdk(client.request)
 // BUILD STATEMENT
 const stmt1: Statement = {
   decryptionKey: stringToTopic("key"),
+  priority: 1,
   topics: [stringToTopic("1"), stringToTopic("2")],
   data: Binary.fromText("TEST 1"),
 }
 
 // SIGN AND SUBMIT STATEMENT
 const signed1 = await alice.sign(stmt1)
-await sdk.submit(signed1)
+console.log(await sdk.submit(signed1))
 
 console.log(
   jsonPrint(
